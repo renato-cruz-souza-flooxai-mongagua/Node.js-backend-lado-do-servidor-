8e8ca95ab848d79c8889ba4576bc4956ec0ed023
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-06 16:47:16 -0300

    feat: Unrecognized URLs

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ let server = https.createServer((req, res)=>{
     console.log('URL:', req.url)
     console.log('METHOD',req.method)
 
-    res.end('OK')
+    switch (req.url){
+        case '/':
+
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text/html')
+        res.end('<h1>Olá</h1>');
+
+        break
+    }
 })
 
 server.listen(3000,'127.0.0.1',()=>{
