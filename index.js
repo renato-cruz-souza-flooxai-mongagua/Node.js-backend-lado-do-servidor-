1341e962d4fe641f2bfbacc208d9a587f9488f19
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-06 17:59:40 -0300

    refactor: improve code readability and organization

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
-const https = require(`http`)
+const express = require(`express`)
 
-let server = https.createServer((req, res)=>{
-    console.log('URL:', req.url)
-    console.log('METHOD',req.method)
+let app = express();
 
-    switch (req.url){
-        case '/':
+app.get('/', (req, res)=>{
 
-        res.statusCode = 200;
+    res.statusCode = 200;
         res.setHeader('content-type', 'text/html')
         res.end('<h1>Olá</h1>');
 
-        break;
+});
 
-        case '/Users':
+app.get('/Users', (req, res)=>{
 
-        res.statusCode = 200;
-        res.setHeader('content-type', 'application/json')
-        res.end(JSON.stringify({
-            users: [{
-                name:'Hcode',
-                email:'contato@hcode.com.br',
-                id:1
-            }]
-        }));
+    res.statusCode = 200;
+    res.setHeader('content-type', 'application/json')
+    res.json({
+        users: [{
+            name:'Hcode',
+            email:'contato@hcode.com.br',
+            id:1
+        }]
+    });
 
-        break;
+});
 
-    }
-})
-
-server.listen(3000,'127.0.0.1',()=>{
-    console.log('servidor rodando')
+app.listen(3000,'127.0.0.1',()=>{
+console.log('servidor rodando')
 }) 
