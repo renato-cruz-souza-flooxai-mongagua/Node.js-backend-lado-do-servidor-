c66f2d8f8dbada1cec74d0121c9d63e2bd5fa1f6
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-07 13:06:00 -0300

    refactor: refactoring routes using consign for modularization

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
-let express = require(`express`);
-let routes = express.Router();
-routes.get('/', (req, res)=>{
+module.exports = (app)=>{
 
-    res.statusCode = 200;
-    res.setHeader('content-type', 'text/html')
-    res.end('<h1>Olá</h1>');
+    app.get('/', (req, res)=>{
 
-});
+        res.statusCode = 200;
+        res.setHeader('content-type', 'text/html')
+        res.end('<h1>Olá</h1>');
+    
+    });
 
-module.exports = routes;
+};
