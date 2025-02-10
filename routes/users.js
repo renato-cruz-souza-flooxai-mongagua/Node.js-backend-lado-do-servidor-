288dc40bb94da7b4f67c47d14f15d2464dc8132e
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-10 13:56:48 -0300

    refactor: cleaner code

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,35 @@
-let NeDB = require('nedb')
-let db = new NeDB ({
-    filename:'users.db',
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
     autoload: true
-})
+});
 
 module.exports = (app) => {
 
-    app.get('/users', (req, res) => {
-        res.status(200).json({
-            users: [{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
-                id: 1
-            }]
+    let route = app.route('/users')
+
+    route.get((req, res) => {
+
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+            } else {
+                res.statusCode = 200;
+                res.setHeader(`content-type`, 'application/json');
+                res.json({
+                    users
+                })
+            }
         });
     });
 
-    app.post('/users', (req, res) => {
+    route.post((req, res) => {
 
         db.insert(req.body, (err, user) =>{
 
             if(err) {
-                console.log(`error: ${err}`)
-                res.status(400).json({
-                    error: err
-                })
+                app.utils.error.send(err, req, res);
             } else {
                 res.status(200).json(user)
             }
