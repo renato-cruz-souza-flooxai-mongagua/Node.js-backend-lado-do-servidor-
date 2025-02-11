9f94fdd1ab4b05f7b8d5aa179c2e8f6142d4403a
renato-cruz-souza-flooxai-mongagua <renato.souza.flooxmongagua@gmail.com>
2025-02-11 10:44:56 -0300

    refactor: simplifying and cleaning the code

--- routes/users.js
+++ routes/users.js
@@ -1,87 +1,95 @@
 let NeDB = require('nedb');
 let db = new NeDB({
-    filename: 'users.db',
-    autoload: true
+    filename:'users.db',
+    autoload:true
 });
 
-module.exports = (app) => {
+module.exports = app => {
 
-    let route = app.route('/users')
+    let route = app.route('/users');
 
     route.get((req, res) => {
 
-        db.find({}).sort({ name: 1 }).exec((err, users) => {
+        db.find({}).sort({name:1}).exec((err, users)=>{
 
             if (err) {
                 app.utils.error.send(err, req, res);
             } else {
+
                 res.statusCode = 200;
-                res.setHeader(`content-type`, 'application/json');
+                res.setHeader('Content-Type', 'application/json');
                 res.json({
                     users
-                })
+                });
+
             }
+
         });
+
     });
 
     route.post((req, res) => {
+        
+        if (!app.utils.validator.user(app, req, res)) return false;
+        
+        db.insert(req.body, (err, user)=>{
 
-        db.insert(req.body, (err, user) =>{
-
-            if(err) {
+            if (err) {
                 app.utils.error.send(err, req, res);
             } else {
-                res.status(200).json(user)
+
+                res.status(200).json(user);
+
             }
 
-        })
+        });
+
     });
 
-    let routeId = app.route('/users/:id')
+    let routeId = app.route('/users/:id');
 
-    routeId.get((req, res)=> {
-        
+    routeId.get((req, res) => {
+
         db.findOne({_id:req.params.id}).exec((err, user)=>{
 
-            if(err) {
+            if (err) {
                 app.utils.error.send(err, req, res);
             } else {
-                res.status(200).json(user)
+                res.status(200).json(user);
             }
 
-        })
+        });
 
-    })
+    });
 
-    routeId.put((req, res)=> {
+    routeId.put((req, res) => {
         
-        db.update({_id:req.params.id}, req.body, err =>{
+        if (!app.utils.validator.user(app, req, res)) return false;
 
-        
+        db.update({ _id: req.params.id }, req.body, err => {
 
-            if(err) {
+            if (err) {
                 app.utils.error.send(err, req, res);
             } else {
-                res.status(200).json(Object.assign(req.params, req.body))
+                res.status(200).json(Object.assign(req.params, req.body));
             }
 
-        })
+        });
 
-    })
-
+    });
     
- // 
-    routeId.delete((req, res)=> {
-        db.remove({_id:req.params.id}, {}, err =>{
-           
-            if(err) {
+    routeId.delete((req, res)=>{
+
+        db.remove({ _id: req.params.id }, {}, err=>{
+
+            if (err) {
                 app.utils.error.send(err, req, res);
             } else {
-                res.status(200).json(req.params)
+                res.status(200).json(req.params);
             }
 
-        })
-    })
+        });
 
+    });
 
 };
